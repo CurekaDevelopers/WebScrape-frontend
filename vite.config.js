9bce57572ffa64fc364e9e892c897acceb940152
CurekaDevelopers <developer1@wedjat.tech>
2025-02-07 17:52:19 +0530

    code update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000, // Ensure it's the correct port
-    host: "0.0.0.0",
-  },
   build: {
-    outDir: "dist",
+    outDir: "dist", // Ensure this is "dist"
   },
 });
